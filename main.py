4c154a0e3f88bc846c83af4dc4e195744b34ee53
Jake Burton <40278490@live.napier.ac.uk>
2021-02-23 22:28:28 +0000

    Tutorial 3 Completed

--- main.py
+++ main.py
@@ -3,23 +3,37 @@ import tcod
 
 from entity import Entity
 from engine import Engine
-from game_map import GameMap
 from input_handlers import EventHandler
+from procgen import generate_dungeon
 
 def main() -> None:
-    screen_width = 80
-    screen_height = 50
-    map_width = 80
-    map_height = 45
+    screen_width = 100
+    screen_height = 70
+    map_width = screen_width
+    map_height = screen_height - 5
+
+    room_max_size = 15
+    room_min_size = 6
+    max_rooms = 30
+
     #Tileset found in the data folder. Standard Ascii pattern letters, could change in future
     tileset = tcod.tileset.load_tilesheet(
         "data\dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
     event_handler = EventHandler()
     player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255,255,255))
-    npc = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255,255,0))
+    npc = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255,0,0))
     entities = {npc, player}
-    game_map = GameMap(map_width, map_height)
+
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
+
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
     #Actually creates the window
     with tcod.context.new_terminal(
